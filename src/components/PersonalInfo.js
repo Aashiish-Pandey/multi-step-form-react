01f783bac0701e171cbc3ef83d452575252c3022
Aashiish-Pandey <ash006pandey@gmail.com>
2022-04-27 00:24:02 +0530

    basic structure

--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -3,25 +3,56 @@ import React from "react";
 function PersonalInfo({ formData, setFormData }) {
   return (
     <div className="personal-info-container">
+      <div>GENERAL INFORMATION</div>
+      <label>Brand Name*</label>
       <input
         type="text"
-        placeholder="First Name..."
+        placeholder="Input Your Brand Name"
         value={formData.firstName}
         onChange={(e) => {
           setFormData({ ...formData, firstName: e.target.value });
         }}
       />
+      <label>Brand Type*</label>
       <input
         type="text"
-        placeholder="Last Name..."
+        placeholder="Select Brand Type"
         value={formData.lastName}
         onChange={(e) => {
           setFormData({ ...formData, lastName: e.target.value });
         }}
       />
+      <label>Street Address*</label>
       <input
         type="text"
-        placeholder="Username..."
+        placeholder="Input Your Street Address"
+        value={formData.username}
+        onChange={(e) => {
+          setFormData({ ...formData, username: e.target.value });
+        }}
+      />
+      <label>City*</label>
+      <input
+        type="text"
+        placeholder="Input City"
+        value={formData.username}
+        onChange={(e) => {
+          setFormData({ ...formData, username: e.target.value });
+        }}
+      />
+      <label>Zip Code*</label>
+      <input
+        type="text"
+        placeholder="Input Zip code"
+        value={formData.username}
+        onChange={(e) => {
+          setFormData({ ...formData, username: e.target.value });
+        }}
+      />
+      <label>Tax ID Number*</label>
+      <input
+        type="text"
+        placeholder="Input Tax Id Number"
         value={formData.username}
         onChange={(e) => {
           setFormData({ ...formData, username: e.target.value });
--- src/components/SignUpInfo.js
+++ src/components/SignUpInfo.js
@@ -3,30 +3,73 @@ import React from "react";
 function SignUpInfo({ formData, setFormData }) {
   return (
     <div className="sign-up-container">
+      <div>
+        <label>First Name*</label>
       <input
         type="text"
-        placeholder="Email..."
+        required
+        placeholder="Input Your First Name"
+        value={formData.firstName}
+        onChange={(e) => {
+          setFormData({ ...formData, firstName: e.target.value });
+        }}
+      />
+      </div>
+      <div>
+      <label>Last Name*</label>
+      <input
+        type="text"
+        placeholder="Input your Last Name"
+        value={formData.lastName}
+        onChange={(e) => {
+          setFormData({ ...formData, lastName: e.target.value });
+        }}
+      />
+      </div>
+      <div>
+      <label>Email*</label>
+      <input
+        type="text"
+        placeholder="Input Your Email"
         value={formData.email}
         onChange={(event) =>
           setFormData({ ...formData, email: event.target.value })
         }
       />
+      </div>
+      <div>
+      <label>Phone Number*</label>
       <input
         type="text"
-        placeholder="Password..."
+        placeholder="Input Your Phone Number"
+        value={formData.email}
+        onChange={(event) =>
+          setFormData({ ...formData, email: event.target.value })
+        }
+      />
+      </div>
+      <div>
+      <label>Password*</label>
+      <input
+        type="text"
+        placeholder="Create Password"
         value={formData.password}
         onChange={(event) =>
           setFormData({ ...formData, password: event.target.value })
         }
       />
+      </div>
+      <div>
+      <label>Confirm Password*</label>
       <input
         type="text"
-        placeholder="Confirm Password..."
+        placeholder="Confirm Your Password"
         value={formData.confirmPassword}
         onChange={(event) =>
           setFormData({ ...formData, confirmPassword: event.target.value })
         }
       />
+      </div>
     </div>
   );
 }
